73aba33b3454282fb0ad317eae459857277071ab
Fitzgerald Afful <fitzafful@gmail.com>
2018-08-06 20:09:16 +0000

    Portfolio start

--- js/main.js
+++ js/main.js
@@ -87,7 +87,7 @@
 
         /** Typed.js (Text typing effect) */
         $("#typed").typed({
-            strings: ["^2100&nbsp;I'm Morano.<br/>UI / UX Designer from Texas."],
+            strings: ["^2100&nbsp;I'm Fitzgerald.<br/>Android / iOS Developer."],
             typeSpeed: 30,
             backDelay: 750,
             loop: false,
